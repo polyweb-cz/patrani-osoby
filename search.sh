5c26e8c09e184308aaa2fea23406f6a171e4b2d5
Mafiosoweb1 <peta.steinbauer@gmail.com>
2023-06-01 16:06:24 +0200

    Fixed ending lines added search.sh input (date name) output json

---
+++ search.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Získání parametrů
+hledane_datum="$1"
+hledane_jmeno="$2"
+treti_parametr="$3"
+
+# Sloučení druhého a třetího parametru s mezerou
+hledane_jmeno="$hledane_jmeno $treti_parametr"
+
+# Hledání řádků, které splňují obě kritéria
+vysledek=$(grep -F ";$hledane_datum;" people.csv | grep -F "$hledane_jmeno")
+
+# Výpis výsledku do konzole
+if [ -n "$vysledek" ]; then
+    echo -n "["
+    first_line=true
+    while IFS= read -r line; do
+        if [ "$first_line" = true ]; then
+            first_line=false
+        else
+            echo -n ","
+        fi
+        gender=$(echo "$line" | cut -d';' -f1)
+        ID=$(echo "$line" | cut -d';' -f2 | tr -d '\n')  # Odstranění nového řádku
+        birth=$(echo "$line" | cut -d';' -f3)
+        name=$(echo "$line" | cut -d';' -f4)
+        state=$(echo "$line" | cut -d';' -f5)
+        link="https://aplikace.policie.cz/patrani-osoby/PersonDetail.aspx?person_id=$ID"
+        echo -n '{"gender":"'"$gender"'","id":"'"$ID"'","birth":"'"$birth"'","name":"'"$name"'","state":"'"$state"'","link":"'"$link"'"}'
+    done <<< "$vysledek"
+    echo "]"
+else
+    echo "Nenalezen žádný řádek splňující zadaná kritéria."
+fi
